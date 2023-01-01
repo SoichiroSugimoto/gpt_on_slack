327486f0df08ff28aad07dcf36e41318b8a1b1b1
杉本創一朗 <sugimotosoichiro@sugimotrounoAir.lan>
2023-01-01 18:34:29 +0900

    add slack functions using slack api

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import slack_functions as slack
+
+def main():
+  channel = "C04G56FP3S7"
+  message = "今年もよろしくお願いします。"
+  res = slack.post_channel_message(channel, message)
+  print (res)
+
+if __name__ == "__main__":
+  main()
---
+++ src/slack_functions.py
@@ -0,0 +1,44 @@
+import requests
+
+slack_bot_token = os.getenv('SLACK_BOT_TOKEN')
+
+def post_channel_message(channel, message):
+  slack_request_headers = {"Authorization": "Bearer " + slack_bot_token}
+  params={
+              "channel": channel,
+              "text": message
+          }
+
+  response = requests.post("https://slack.com/api/chat.postMessage", headers=slack_request_headers, params=params)
+  return (response.json())
+
+def post_channel_reply(channel, message, ts):
+  slack_request_headers = {"Authorization": "Bearer " + slack_bot_token}
+  params={
+              "channel": channel,
+              "text": message,
+              "channel": channel,
+              "thread_ts": ts
+          }
+
+  response = requests.post("https://slack.com/api/chat.postMessage", headers=slack_request_headers, params=params)
+  return (response.json())
+
+def get_channel_message(channel):
+  slack_request_headers = {"Authorization": "Bearer " + slack_bot_token}
+  params = {
+    "channel": channel
+  }
+
+  response = requests.get("https://slack.com/api/conversations.history", headers=slack_request_headers, params=params)
+  return (response.json())
+
+def get_channel_thread(channel, ts):
+  slack_request_headers = {"Authorization": "Bearer " + slack_bot_token}
+  params = {
+    "channel": channel,
+    "ts": ts
+  }
+
+  response = requests.get("https://slack.com/api/conversations.replies", headers=slack_request_headers, params=params)
+  return (response.json())
