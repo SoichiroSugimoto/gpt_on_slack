86ff3bb636c0715d3ec6e9984b776b62bde7ea44
杉本創一朗 <sugimotosoichiro@sugimotrounoAir.lan>
2023-01-03 15:21:46 +0900

    simple reply

--- src/interactive_slack_chat_bot/lambda_function.py
+++ src/interactive_slack_chat_bot/lambda_function.py
@@ -12,11 +12,8 @@ from pynamodb.attributes import UnicodeAttribute, NumberAttribute, MapAttribute
 dynamodb = boto3.resource('dynamodb', region_name = 'ap-northeast-1')
 conversations = dynamodb.Table('conversation')
 
-
-def lambda_handler(event, context):
+def usage_guide(receive_payload):
   channel = "C04G56FP3S7"
-  receive_body = parse.parse_qs(event['body'])
-  receive_payload = json.loads(receive_body["payload"][0])
   if (receive_payload['type'] == "shortcut" and receive_payload['callback_id'] == "shortcut_0000"):
     message = conversations.get_item( Key={'message_id':'m1'} )['Item']['message']
     value = json.dumps(json.loads(message)["blocks"])
@@ -33,5 +30,27 @@ def lambda_handler(event, context):
     res = slack.post_channel_by_params(channel, {"text": "未実装"})
   else:
     res = slack.post_channel_by_params(channel, {"text": "Out of Scope"})
-    
   return (res)
+
+def lambda_handler(event, context):
+  try:
+    receive_body = parse.parse_qs(event['body'])
+    receive_payload = json.loads(receive_body["payload"][0])
+  except:
+    receive_payload = json.loads(event['body'])
+  retry_num = int(json.loads(event['headers']['X-Slack-Retry-Num']))
+  if (retry_num >= 2):
+    return (None)
+  if (receive_payload['type'] == 'url_verification'):
+    return {
+      'statusCode': 200,
+      'body': json.dumps( {'challenge': receive_payload['challenge'] } )
+    }
+  elif (receive_payload["type"] == "shortcut" or receive_payload["type"] == "block_actions"):
+    response = usage_guide(event)
+  elif (receive_payload["type"] == "event_callback" and receive_payload["event"]["user"] != "U04HAFAP9FW" and retry_num == 1):
+    response = slack.post_channel_reply("C04G56FP3S7", "こんにちは。", receive_payload["event"]["ts"])
+  else:
+    response == None
+  return (response)
+    
